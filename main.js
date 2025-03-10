4c1e08c8a7613d71e135209c6f36c1cd993c983a
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2025-03-10 16:35:03 +0200

    lesson-2

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-
 const express = require('express');
 const {userService} = require("./services/user.service");
 
 const app = express();
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended: true}))
 
 // app.get('/users/:name', (req, res)=>{
 //     console.log(req.params.name);
@@ -30,22 +29,24 @@ app.use(express.urlencoded({extended:true}))
 //     res.end('Hello from delete')
 // })
 
-app.get('/users', async (req, res)=>{
+app.get('/users', async (req, res) => {
     const data = await userService.getAll();
     res.json(data)
 })
-app.get('/users/:id', async (req, res)=>{
+
+app.get('/users/:id', async (req, res) => {
     const id = req.params.id;
     const data = await userService.getById(id);
     res.json(data)
 })
-app.post('/users', async (req, res)=>{
+
+app.post('/users', async (req, res) => {
     const user = req.body;
     const data = await userService.create(user);
     res.json(data)
 })
 
-app.listen(5000, ()=>{
+app.listen(5000, () => {
     console.log('server running on 5000 port');
 })
 
