6a4190a41d1becfa36f335e3b79c22fa6517cd2b
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2025-03-10 15:54:17 +0200

    lesson_1

--- main.js
+++ main.js
@@ -84,6 +84,89 @@
 // fs
 ////////////////////////////////////////////////////////////
 
+// const afs = require('node:fs/promises');
+// const fs = require('node:fs');
+// const readline = require('node:readline/promises');
+//
+// const path = require('node:path');
+//
+// const start = async ()=>{
+//     // await fs.mkdir(path.join('storage', 'files'), {recursive:true})
+//     const filePath = path.join('storage', 'addddd.txt');
+//     // await fs.writeFile(filePath, 'Hello1\n')
+//     // await fs.appendFile(filePath, 'Hello2\n')
+//     // const arrayBufferLikeBuffer = await fs.readFile(filePath, {encoding:'utf-8'});
+//     // console.log(arrayBufferLikeBuffer);
+//     // await fs.rename(filePath, path.join(process.cwd(),'storage', 'asd','myFile2.txt'))
+//     // await fs.rename(filePath, path.join(path.dirname(filePath), 'addddd.txt'))
+//     // await fs.copyFile(filePath, path.join(path.dirname(filePath),'MyFile.txt') )
+//     // await fs.rm(path.join(process.cwd(), 'storage'), {recursive:true})
+//     // await fs.unlink('1111.txt')
+//     // const stats = await fs.stat('services');
+//     // console.log(stats.isDirectory());
+//
+//     // const fileStream = fs.createReadStream(filePath, 'utf-8');
+//     // const rl = readline.createInterface({input:fileStream});
+//     // try {
+//     //     for await (const line of rl){
+//     //         await afs.appendFile('res.txt', `${line}-----------\n`)
+//     //     }
+//     // }finally {
+//     //     await rl.close()
+//     // }
+//     // const readStream = fs.createReadStream('kostyli.jpg');
+//     // const writeStream = fs.createWriteStream('123.jpg');
+//     // // readStream.on('data', (chunk)=>{
+//     // //     writeStream.write(chunk)
+//     // // })
+//     // readStream.pipe(writeStream)
+// }
+//
+// start()
 
 
+
+//////////////////////////////////////////////////////////
+// os
+//////////////////////////////////////////////////////////
+
+// const os = require('node:os');
+//
+// console.log(os.arch());
+// console.log(os.cpus());
+// console.log(os.totalmem()/1024/1024/1024);
+// console.log(os.freemem()/1024/1024/1024);
+// console.log(os.homedir());
+// console.log(os.hostname());
+// console.log(os.release());
+// console.log(os.tmpdir());
+// console.log(os.type());
+// console.log(os.uptime());
+// console.log(os.userInfo());
+// console.log(os.version());
+// console.log(os.networkInterfaces());
+// console.log(os.platform());
+
+//////////////////////////////////////////////////////////
+// events
+//////////////////////////////////////////////////////////
+
+const emitter = require('node:events');
+
+const em = new emitter.EventEmitter();
+
+em.on('fcall', ()=>{
+    console.log('fcall');
+})
+
+// em.on('scall', (name, age)=>{
+//     console.log('scall',name, age);
+// })
+em.once('scall', (name, age)=>{
+    console.log('scall',name, age);
+})
+
+em.emit('scall', 'Max', 15)
+em.emit('scall', 'Max', 15)
+em.emit('scall', 'Max', 15)
 
