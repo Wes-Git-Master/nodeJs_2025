908a2868ae79195f24a16aaed30d065227b2a2ad
Wes-Git-Master <158311677+Wes-Git-Master@users.noreply.github.com>
2025-03-13 17:44:40 +0200

    express/lesson-3

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import express, {Request,Response} from 'express';
 import * as mongoose from "mongoose";
 import {userService} from "./services/user.service";
 import {IUserDTO} from "./interfaces/user.interface";
+import {config} from "./configs/config";
 
 const app = express();
 app.use(express.json());
@@ -32,7 +33,7 @@ const dbConnection = async (): Promise<void> => {
     while (!dbCon){
         try {
             console.log('Connecting to db ...');
-            await mongoose.connect('mongodb+srv://edvinfaas:1qwe2asd3zxc@cluster0.ji8p7.mongodb.net/nodeJs-express-db-2025')
+            await mongoose.connect(config.MONGODB_URI)
             dbCon = true;
             console.log('Database available !!!');
 
@@ -46,8 +47,8 @@ const dbConnection = async (): Promise<void> => {
 const start = async  (): Promise<void> => {
     try {
         await dbConnection();
-        app.listen(2222, () => {
-            console.log('Server started on port 2222');
+        app.listen(config.PORT, () => {
+            console.log(`Server started on port ${config.PORT}`);
         })
     }
     catch (e) {
